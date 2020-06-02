d4b47fa5a741b9a390c77f5b86c80de5f71a26bc
cjiabka <cjiabka86@gmail.com>
2020-06-02 14:03:14 -0700

    Replace part of code

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_crontab import Crontab
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
+
 
 appF = Flask(__name__)
 appF.config.from_object(Config)
@@ -16,30 +14,5 @@ login = LoginManager(appF)
 login.login_view = 'login'
 #crontab = Crontab(appF)
 
-if not appF.debug:
-    if appF.config['MAIL_SERVER']:
-        auth = None
-        if appF.config['MAIL_USERNAME'] or appF.config['MAIL_PASSWORD']:
-            auth = (appF.config['MAIL_USERNAME'], appF.config['MAIL_PASSWORD'])
-        secure = None
-        if appF.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(appF.config['MAIL_SERVER'], appF.config['MAIL_PORT']),
-            fromaddr='no-reply@' + appF.config['MAIL_SERVER'],
-            toaddrs=appF.config['ADMINS'], subject='Service Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        appF.logger.addHandler(mail_handler)
-
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/gss_service.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        appF.logger.addHandler(file_handler)
-
-        appF.logger.setLevel(logging.INFO)
-        appF.logger.info('Service startup')
 
 from app import routes, models, errors
--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import appF, db
-
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 @appF.errorhandler(404)
 def not_found_error(error):
@@ -11,3 +13,30 @@ def not_found_error(error):
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
+
+if not appF.debug:
+    if appF.config['MAIL_SERVER']:
+        auth = None
+        if appF.config['MAIL_USERNAME'] or appF.config['MAIL_PASSWORD']:
+            auth = (appF.config['MAIL_USERNAME'], appF.config['MAIL_PASSWORD'])
+        secure = None
+        if appF.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(appF.config['MAIL_SERVER'], appF.config['MAIL_PORT']),
+            fromaddr='no-reply@' + appF.config['MAIL_SERVER'],
+            toaddrs=appF.config['ADMINS'], subject='Service Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        appF.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/gss_service.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    appF.logger.addHandler(file_handler)
+
+    appF.logger.setLevel(logging.INFO)
+    appF.logger.info('Service startup')
