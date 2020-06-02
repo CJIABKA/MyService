9bcf0482b6717e88dc1fa2d91424f3c3787c669c
cjiabka <cjiabka86@gmail.com>
2020-06-02 14:01:35 -0700

    Connect to MSSQL

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+from urllib import parse
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'just-some-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://sa:Testing1122@localhost:1433/TestDB?driver=ODBC+Driver+17+for+SQL+Server"
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
